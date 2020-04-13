7524b67b7900ebfea4bb2ffd771c7dec24685edc
hacker_tesla <hacker_tesla@protonmail.com>
2020-04-14 01:18:49 +0600

    try to delete previouse file

--- main_app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, jsonify, flash, redirect, url_for, request
-from main_app import app
-
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
